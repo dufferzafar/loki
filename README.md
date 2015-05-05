1e985c80d9b761e9e381928e361b4f622fffb19d
Shadab Zafar <dufferzafar0@gmail.com>
2015-05-05 20:15:09 +0530

    Version 0.3
    Created in a stretch of 6 hours on Tue, 05 May 2015 from 2 to 8 PM.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # loki
+
 PyQt based frontend for locate tool.
+
+Initial version created in a stretch of 6 hours on Tue, 05 May 2015 from 2 to 8 PM.
+
+![Screenshot](screenshot.png)
+
+# Hotkeys
+
+`Ctrl + F` - Focus search bar
+
+`Ctrl + L` - Clear all items
+
+`Ctrl + Q` - Quit application
+
+# Todo
+
+* Go through Catfish's source
+
+* File Icons
+
+* Restrict search to a particular folder
+
+* Tab order
+* Pressing enter on an item opens it
+* Context menu to open folder (ctrl+enter)
+
+* Advanced query syntax?
+  * size > 1 M
+  * modified < 2 Weeks
+
+# Similar Stuff
+
+* [Catfish](https://help.ubuntu.com/community/Catfish)
+
+* [Everything for Windows](http://www.voidtools.com/)
---
+++ loki.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+import os
+import sys
+import subprocess
+
+
+from PyQt4 import uic
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+# Handle Ctrl + C
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+# Load the UI
+form_class = uic.loadUiType("main.ui")[0]
+
+
+def sizeof_fmt(num, suffix='B'):
+    """
+    Convert Bytes to Human Readable Form: KB, MB etc.
+
+    http://stackoverflow.com/a/1094933/2043048
+    """
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f %s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f %s%s" % (num, 'Y', suffix)
+
+
+class loki(QMainWindow, form_class):
+
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+
+        self.itemCount = 0
+
+        # Status Bar
+        self.status = self.statusBar()
+        self.status.showMessage("Ready")
+
+        self.itemStatus = QLabel("%d Items" % self.itemCount)
+        self.status.addPermanentWidget(self.itemStatus)
+
+        self.searchBtn.clicked.connect(self.search)
+        self.query.returnPressed.connect(self.searchBtn.click)
+
+        # Todo: Add these things to Menus too?
+        # http://zetcode.com/gui/pyqt4/menusandtoolbars/
+        QShortcut(QKeySequence("Ctrl+Q"), self, self.close)
+        QShortcut(QKeySequence("Ctrl+F"), self, self.query.setFocus)
+        QShortcut(QKeySequence("Ctrl+L"), self, self.results.clear)
+
+        # Query from command line
+        if len(sys.argv) >= 2:
+            self.query.setText(sys.argv[1])
+            # Todo: Display window and begin searching
+            # self.search()
+
+    def search(self):
+        """ Run locate command and display results. """
+
+        # Empty query
+        query = self.query.text().strip()
+        if not query:
+            return
+
+        # Set working status
+        self.results.clear()
+        self.status.showMessage("Searching...")
+        self.results.setSortingEnabled(False)
+        self.repaint()
+
+        # Launch Command
+        locate = subprocess.Popen(['locate', query, '-l', '10000', '--existing'],
+                                  stdout=subprocess.PIPE)
+
+        items = []
+        count = 0
+        while locate.poll() is None:
+            path = locate.stdout.readline().decode("utf-8").strip()
+            parts = os.path.split(path)
+
+            # Skip non existent files
+            try:
+                info = os.stat(path)
+            except FileNotFoundError:
+                continue
+
+            item = QTreeWidgetItem()
+
+            # Name & Path
+            item.setText(0, parts[1])
+            item.setText(1, parts[0])
+
+            # Size & Date Modified
+            item.setText(2, sizeof_fmt(info.st_size))
+            time = datetime.utcfromtimestamp(
+                info.st_mtime).strftime('%d/%m/%Y %H:%M:%S')
+            item.setText(3, time)
+
+            item.setSizeHint(0, QSize(450, 20))
+            item.setSizeHint(1, QSize(550, 20))
+            item.setSizeHint(2, QSize(75, 20))
+            item.setSizeHint(3, QSize(200, 20))
+
+            items.append(item)
+
+            # Todo: Improve
+            if len(items) == 500:
+                self.results.insertTopLevelItems(count, items)
+                self.results.resizeColumnToContents(0)
+                self.results.resizeColumnToContents(1)
+                self.results.resizeColumnToContents(2)
+                self.results.resizeColumnToContents(3)
+                self.results.repaint()
+                items = []
+                count += 500
+
+        if items:
+            self.results.insertTopLevelItems(count, items)
+
+        # Resize Columns
+        self.results.resizeColumnToContents(0)
+        self.results.resizeColumnToContents(1)
+        self.results.resizeColumnToContents(2)
+        self.results.resizeColumnToContents(3)
+
+        self.status.showMessage("Ready")
+
+        self.itemCount = self.results.topLevelItemCount()
+        self.itemStatus.setText("%d Items" % self.itemCount)
+
+        # Sort by Name
+        self.results.setSortingEnabled(True)
+        if self.itemCount < 10000:
+            self.results.sortByColumn(0, 0)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = loki(None)
+    window.showMaximized()
+
+    app.exec_()
